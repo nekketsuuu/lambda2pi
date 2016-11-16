255f0573351655ff47ed0963a21859afeaba4197
nekketsuuu <nekketsuuu@users.noreply.github.com>
2016-11-17 00:28:46 +0900

    Add tests for parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -29,4 +29,68 @@ func TestParseExpr(t *testing.T) {
 	if _, ok := x.(lambda2pi.LAbs); !ok {
 		t.Errorf("ParseExpr(\"%v\"): got %T, want lambda2pi.LAbs", e, x)
 	}
+
+	// parenthesis
+	e = "(x)"
+	x, err = ParseExpr(e)
+	if err != nil {
+		t.Errorf("ParseExpr(%q): %v", e, err)
+	}
+	// sanity check
+	if _, ok := x.(lambda2pi.LVar); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T, want lambda2pi.LVar", e, x)
+	}
+
+	// application 1
+	e = "x y"
+	x, err = ParseExpr(e)
+	if err != nil {
+		t.Errorf("ParseExpr(%q): %v", e, err)
+	}
+	// sanity check
+	if _, ok := x.(lambda2pi.LApp); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T, want lambda2pi.LApp", e, x)
+	}
+
+	// application 2
+	e = "(\\x. x) y"
+	x, err = ParseExpr(e)
+	if err != nil {
+		t.Errorf("ParseExpr(%q): %v", e, err)
+	}
+	// sanity check
+	if _, ok := x.(lambda2pi.LApp); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T, want lambda2pi.LApp", e, x)
+	}
+	if _, ok := x.(lambda2pi.LApp).First.(lambda2pi.LAbs); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of (\\x. x), want lambda2pi.LAbs", e, x)
+	}
+
+	// application 3
+	e = "(\\x. \\y. x y) y z"
+	x, err = ParseExpr(e)
+	if err != nil {
+		t.Errorf("ParseExpr(%q): %v", e, err)
+	}
+	// sanity check
+	var xx lambda2pi.Lambda
+	var ok bool
+	if xx, ok = x.(lambda2pi.LApp); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T, want lambda2pi.LApp", e, x)
+	}
+	if _, ok = xx.(lambda2pi.LApp).Second.(lambda2pi.LVar); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of \"z\", want lambda2pi.LVar", e, x)
+	}
+	if xx, ok = xx.(lambda2pi.LApp).First.(lambda2pi.LApp); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of \"%v\", want lambda2pi.LApp", e, x, xx.String())
+	}
+	if xx, ok = xx.(lambda2pi.LApp).First.(lambda2pi.LAbs); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of \"%v\", want lambda2pi.LAbs", e, x, xx.String())
+	}
+	if xx, ok = xx.(lambda2pi.LAbs).Body.(lambda2pi.LAbs); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of \"%v\", want lambda2pi.LAbs", e, x, xx.String())
+	}
+	if _, ok = xx.(lambda2pi.LAbs).Body.(lambda2pi.LApp); !ok {
+		t.Errorf("ParseExpr(\"%v\"): got %T for the type of \"%v\", want lambda2pi.LApp", e, x, xx.String())
+	}
 }
