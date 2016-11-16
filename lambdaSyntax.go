f4dac4009e90c2f4f2b1320b599cf67261abe08d
nekketsuuu <nekketsuuu@users.noreply.github.com>
2016-11-17 00:28:17 +0900

    Add String() for the type Lambda

--- lambdaSyntax.go
+++ lambdaSyntax.go
@@ -3,8 +3,13 @@ package lambda2pi
 // A type of identifiers
 type LambdaIdent string
 
+func (id LambdaIdent) String() string {
+	return string(id)
+}
+
 // A type of lambda terms
 type Lambda interface {
+	String() string
 	lambda()
 }
 
@@ -30,3 +35,16 @@ type (
 func (t LVar) lambda() {}
 func (t LAbs) lambda() {}
 func (t LApp) lambda() {}
+
+// For config: lambda letter for abstractions
+const lambda string = "\\"
+
+func (t LVar) String() string {
+	return t.Name.String()
+}
+func (t LAbs) String() string {
+	return "(" + lambda + t.Var.String() + ". " + t.Body.String() + ")"
+}
+func (t LApp) String() string {
+	return "(" + t.First.String() + " " + t.Second.String() + ")"
+}
